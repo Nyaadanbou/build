3b628af8ec08d6bb2971eb47ba3c518fab1a41e2
g2213swo <2057581537@qq.com>
2024-03-17 20:44:34 +0800

    update catalog: add hephaestus
    (cherry picked from commit 04b4de272764d18b832352c59759c67fbc8a9b1f)

---
+++ bom/hephaestus/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    `java-platform`
+}
+
+javaPlatform {
+    allowDependencies()
+}
+
+dependencies {
+    api(libs.hephaestus.core) {
+        exclude("net.kyori")
+        exclude("org.jetbrains", "annotations")
+    }
+    api(libs.hephaestus.reader.blockbench) {
+        exclude("com.google.code.gson")
+    }
+    api(libs.hephaestus.runtime.bukkit)
+    // api(libs.hephaestus.runtime.bukkit.adapt) {
+    // }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("creativePlatform") {
+            from(components["javaPlatform"])
+            groupId = "cc.mewcraft.bom"
+            artifactId = "hephaestus"
+            version = "1.0"
+        }
+    }
+}
