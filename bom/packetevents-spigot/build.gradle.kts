b289ae2deadc70a2c445e7698c08d6cfa12fcd59
g2213swo <2057581537@qq.com>
2024-03-26 20:47:21 +0800

    update catalog: add packetevents bom

---
+++ bom/packetevents-spigot/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    `java-platform`
+}
+
+javaPlatform {
+    allowDependencies()
+}
+
+dependencies {
+    api(libs.packetevents.spigot) {
+        exclude("net.kyori", "adventure-api")
+        exclude("net.kyori", "adventure-text-serializer-gson")
+        exclude("net.kyori", "adventure-text-serializer-legacy")
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("creativePlatform") {
+            from(components["javaPlatform"])
+            groupId = "cc.mewcraft.bom"
+            artifactId = "packetevents-spigot"
+            version = "1.0"
+        }
+    }
+}
