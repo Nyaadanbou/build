0e658491a2bfca4dfab017d95098f998a4489705
g2213swo <2057581537@qq.com>
2024-12-28 23:16:21 +0800

    release catalog 0.1

--- catalog/build.gradle.kts
+++ catalog/build.gradle.kts
@@ -25,7 +25,7 @@ publishing {
             from(components["versionCatalog"])
             group = "cc.mewcraft.gradle"
             artifactId = "catalog"
-            version = "1.0-SNAPSHOT"
+            version = "0.1"
             description = "Shared version catalogs"
         }
     }
